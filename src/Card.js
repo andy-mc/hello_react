8be0fd0956bce6381c2efd4a52a3e584c4e72019
Andrés Muñoz <asmz5488@gmail.com>
2019-04-21 14:29:59 -0500

    add robo cards

--- src/Card.js
+++ src/Card.js
@@ -1,20 +1,20 @@
 import React from "react";
 
-const Card = () => {
+const Card = (props) => {
   return (
     <div className="br2 ba dark-gray bg-light-blue hover-bg-light-purple b--black-10 mv4 w-100 w-50-m w-25-l mw5 center">
       <img
-        src="https://robohash.org/test?200x200"
+        src={`https://robohash.org/${props.id}?200x200`}
         className="db w-100 br2 br--top"
         alt="Robot"
       />
       <div className="pa2 ph3-ns pb3-ns">
         <div className="tc w-100 mt1">
           <div className="tc">
-            <h1 className="f5 f4-ns mv0">Cat</h1>
+            <h1 className="f5 f4-ns mv0">{props.name}</h1>
           </div>
         </div>
-        <p className="tc f6 lh-copy measure mt2 mid-gray">email@email.com</p>
+        <p className="tc f6 lh-copy measure mt2 mid-gray">{props.email}</p>
       </div>
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -3,9 +3,17 @@ import React from "react";
 import ReactDOM from "react-dom";
 import * as serviceWorker from "./serviceWorker";
 import Card from "./Card";
+import {robots} from "./robots";
 import "./index.css";
 
-ReactDOM.render(<Card />, document.getElementById("root"));
+ReactDOM.render(
+  <div>
+    {robots.map((robot) => { 
+      return <Card key={robot.id} id={robot.id} name={robot.name}
+              username={robot.username} email={robot.email} />;
+    })}
+  </div>
+  , document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
