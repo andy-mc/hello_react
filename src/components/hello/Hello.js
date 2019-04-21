362b6ff43528606486c40ba755b3ec6717082d97
Andrés Muñoz <asmz5488@gmail.com>
2019-04-20 20:10:07 -0500

    App init

---
+++ src/components/hello/Hello.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './Hello.css';
+
+class Hello extends Component {
+  render() {
+    return (
+      <div className="Header">
+        <h1>Hello World !!</h1>
+      </div>
+    );
+  }
+}
+
+export default Hello;
