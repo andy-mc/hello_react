953f0000f8b948d8bb0c05099777ff7a6e0f3d40
Andrés Muñoz <asmz5488@gmail.com>
2019-04-21 19:17:32 -0500

    Implement cardList in a separte isolated component

--- src/Card.js
+++ src/Card.js
@@ -2,12 +2,13 @@ import React from "react";
 
 const Card = ({id, name, email}) => {
   return (
-    <div className="bg-light-green dib br4 pa3 ma2 grow tc bw2 shadow-5">
+    <div className="bg-light-green dib br4 pa3 ma2 grow tc bw2
+     shadow-5">
       <img src={`https://robohash.org/${id}?200x200`}
-           className="" alt="Robot"/>
-      <div className="">
-        <h1 className="">{name}</h1>
-        <p className="">{email}</p>
+           alt="Robot"/>
+      <div>
+        <h1>{name}</h1>
+        <p>{email}</p>
       </div>
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -2,18 +2,14 @@ import "tachyons";
 import React from "react";
 import ReactDOM from "react-dom";
 import * as serviceWorker from "./serviceWorker";
-import Card from "./Card";
+import CardList from "./CardList";
 import {robots} from "./robots";
 import "./index.css";
 
 ReactDOM.render(
-  <div>
-    {robots.map((robot) => { 
-      return <Card key={robot.id} id={robot.id} name={robot.name}
-              username={robot.username} email={robot.email} />;
-    })}
-  </div>
-  , document.getElementById("root"));
+  <CardList robots={robots} />,
+  document.getElementById("root")
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
