80f566ec0ce4968828310b7d6ac2a1ef74f6dffe
Andrés Muñoz <asmz5488@gmail.com>
2019-04-28 13:44:11 -0500

    add input text for robo search

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from "react";
 import CardList from "./CardList";
+import SearchRobots from "./SearchRobots";
 import {robots} from "./robots";
 
 const App = () => {
   return (
-    <div>
+    <div className="bg-green">
+      <h1 className="tc pa4">ROBOFRIENDS</h1>
+      <SearchRobots / >
       <CardList robots={robots} />
     </div>
   );
---
+++ src/SearchRobots.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const SearchRobots = () => {
+  return (
+    <div className="tc bg-green ma4">
+      <input className="bg-light-gray br4 pa2 w-50" type="text"  placeholder="Search Robot"/>
+    </div>
+  );
+};
+
+export default SearchRobots;
