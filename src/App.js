9e22b49f7b0f1dbf382c2e40befd00a6cd54efe8
Andrés Muñoz <asmz5488@gmail.com>
2019-04-28 13:53:11 -0500

    comunicate searchRobots component to parent using props

--- src/App.js
+++ src/App.js
@@ -1,16 +1,26 @@
-import React from "react";
+import React, {Component} from "react";
 import CardList from "./CardList";
 import SearchRobots from "./SearchRobots";
-import {robots} from "./robots";
+import { robots } from "./robots";
 
-const App = () => {
-  return (
-    <div className="bg-green">
-      <h1 className="tc pa4">ROBOFRIENDS</h1>
-      <SearchRobots / >
-      <CardList robots={robots} />
-    </div>
-  );
+class App extends Component {
+  constructor() { 
+    super();
+  }
+
+  filterRobots = () => { 
+    console.log('sad');
+  }
+  
+  render() { 
+    return (
+      <div className="bg-green">
+        <h1 className="tc pa4">ROBOFRIENDS</h1>
+        <SearchRobots filterRobots={this.filterRobots} / >
+        <CardList robots={robots} />
+      </div>
+    );
+  }
 };
 
 export default App;
--- src/SearchRobots.js
+++ src/SearchRobots.js
@@ -1,9 +1,14 @@
 import React from "react";
 
-const SearchRobots = () => {
+const SearchRobots = ({filterRobots}) => {
   return (
     <div className="tc bg-green ma4">
-      <input className="bg-light-gray br4 pa2 w-50" type="text"  placeholder="Search Robot"/>
+      <input
+        onChange={filterRobots}
+        type="text" 
+        className="bg-light-gray br4 pa2 w-50"
+        placeholder="Search Robot" 
+      />
     </div>
   );
 };
