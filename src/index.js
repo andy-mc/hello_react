df856fa9aed1ec1d8c42bbe82b65a0b5a6c42eb5
Andrés Muñoz <asmz5488@gmail.com>
2019-05-05 14:23:54 -0500

    create store and provde it to the app

--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
 import "tachyons";
 import React from "react";
 import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { createStore } from "redux";
 import * as serviceWorker from "./serviceWorker";
 import App from "./containers/App";
+import { searchRobots } from "./reducers";
 import "./index.css";
 
+const store = createStore(searchRobots);
+
 ReactDOM.render(
-  <App />,
+  <Provider  store={store} >
+    <App />
+  </Provider>,
   document.getElementById("root")
 );
 
--- src/reducers.js
+++ src/reducers.js
@@ -4,12 +4,17 @@ const initialState = {
   searchField: ''
 }
 
-export const searchRobots = (state=initialState, action={}) => {
+export const searchRobots = (state = initialState, action = {}) => {
+  console.log(1);
+  console.log('state:', state)
+  console.log('action:', action)
   switch (action.type) { 
     case CHANGE_SEARCH_FIELD:
+      console.log('CHANGE_SEARCH_FIELD')
       return Object.assign({}, state, { searchField: action.payload })
     
     default:
+      console.log('default:')
       return state;
   }  
 };
